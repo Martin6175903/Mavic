b6550eeba2a7124d7a47a9b6cd17d80a70696512
Martin6175903 <martin6175903@yandex.ru>
2022-08-09 14:28:22 +0300

    end layout

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/fullpage.js/dist/fullpage.js',
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
@@ -51,6 +52,7 @@ function scripts() {
 function styles() {
     return src([
         'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/fullpage.js/dist/fullpage.css',
         'app/scss/style.scss'])
         .pipe(scss({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
