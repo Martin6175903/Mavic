03fa50262a39458e698ad4dbfb1f05522104cfe5
Martin6175903 <martin6175903@yandex.ru>
2022-08-26 16:15:25 +0300

    End First

--- app/js/main.js
+++ app/js/main.js
@@ -17,8 +17,22 @@ $(function () {
 		//options here
 		autoScrolling:true,
 		scrollHorizontally: true,
-      sectionSelector: '.page-section'
+      sectionSelector: '.page-section',
+      menu: '#header__nav',
+      anchors: ['top', 'products', 'benefits', 'specification', 'questions', 'contacts']
 	});
+
+   $('.menu__btn').on('click', function () {
+      $('.menu__btn').toggleClass('menu__btn--active');
+      $('.menu__list').toggleClass('menu__list--active');
+   });
+
+   $('.menu__list-link').on('click', function () {
+      $('.menu__btn').removeClass('menu__btn--active');
+      $('.menu__list').removeClass('menu__list--active');
+   });
+
+
 
 });
 
